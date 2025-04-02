2986747cad639ae471b5bc4b018ad95b0f2b5eef
gordonzu <gordonzu@outlook.com>
2025-04-02 17:25:41 -0400

    add autocmd to cancel italics

--- after/plugin/options.lua
+++ after/plugin/options.lua
@@ -11,34 +11,16 @@ vim.api.nvim_create_autocmd("BufEnter", {
     end,
 })
 
-vim.cmd([[
-  hi Comment cterm=NONE gui=NONE
-  hi Keyword cterm=NONE gui=NONE
-  hi Type cterm=NONE gui=NONE
-  hi Function cterm=NONE gui=NONE
-]])
-
---[[ vim.api.nvim_create_autocmd("BufEnter", {
-  pattern = "*.scala",
-  callback = function()
-    vim.cmd "set tabstop=4"
-    vim.cmd "set shiftwidth=4"
-  end,
-})
-
 vim.api.nvim_create_autocmd("BufEnter", {
-  pattern = "*.html",
-  callback = function()
-    vim.cmd "set tabstop=4"
-    vim.cmd "set shiftwidth=4"
-  end,
+    callback = function()
+        vim.cmd([[
+            hi Comment cterm=NONE gui=NONE
+            hi Keyword cterm=NONE gui=NONE
+            hi Type cterm=NONE gui=NONE
+            hi Function cterm=NONE gui=NONE
+        ]])
+    end,
 })
 
-vim.api.nvim_create_autocmd("BufEnter", {
-  pattern = "*.sbt",
-  callback = function()
-    vim.cmd "set tabstop=4"
-    vim.cmd "set shiftwidth=4"
-  end,
-})
-]]--
+
+
